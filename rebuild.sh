e87404f963a729a83af99fb5a0525cffbaf6ef70
Chris Prather <chris@prather.org>
2017-03-03 01:14:50 -0500

    disable JS building in the rebuild helper

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd std_utils/
-./configure 
+./configure
 cd ..
 
 autoreconf -i
 #./configure --enable-caiml --enable-pcrecpp
-./configure --enable-caiml --enable-aisl --enable-pcrecpp --enable-javascript --with-v8-path=/home/varol/projects/v8
+./configure --disable-werror --enable-caiml --enable-aisl --enable-pcrecpp
 make
 
